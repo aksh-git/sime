16da788fa808407c22abae03e5d1e05e029223aa
aksh-git <ann0m@protonmail.com>
2022-09-15 16:20:33 +0530

    editor page and range comp.

---
+++ components/RangeSlider.js
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from "react";
+import { StateContext } from "../pages/_app";
+
+function RangeSlider({action, min, max, label, name, def}) {
+
+  const { state, dispatch } = useContext(StateContext);
+  const [rvalue, setrvalue] = useState(0);
+
+  function handleChange(e){
+    let v = parseInt(e.target.value)
+    setrvalue(v<min||v>max?rvalue:v)
+  }
+
+  const update=()=>{
+    dispatch({
+      type:action,
+      payload:{
+        title:name,
+        value:rvalue
+      }
+    })
+  }
+
+  useEffect(()=>{
+    update()
+  },[rvalue])
+
+
+  return (
+    <div className="w-full py-1 px-2">
+        <label htmlFor={name} className="font-bold text-gray-600">{label}</label>
+        <div className="w-full flex items-center gap-3 pad-1">
+            <input type="number" onInput={handleChange} className='w-16 text-white text-xl rounded-xl bg-transp pad-1 text-center' value={rvalue}/>
+            <input type="range" onChange={handleChange} value={rvalue} min={min}name={name} max={max} className="range range-xs range-accent dark:range-dark" /> 
+        </div>
+    </div>
+  )
+}
+
+export default RangeSlider
--- pages/editor.js
+++ pages/editor.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
-import { StateContext } from "./_app";
-import { rawImageData } from "./_app";
+import { StateContext } from "../pages/_app";
+// import { rawImageData } from "./_app";
+import styles from '../styles/Editor.module.css';
+import RangeSlider from "../components/RangeSlider";
 
 function editor() {
 
@@ -24,12 +26,27 @@ function editor() {
   },[imageURL])
   return (
     <>
-      {!state.editing &&
+      {/* {!state.editing &&
         <div align="center" className="text-white"><h1>Hello editor!!</h1><br/><h3>We've to get an image to work on it.</h3></div>
       }
       {mounted && <div>
         <img src={imageURL} />
-      </div>}
+      </div>} */}
+      <div className={styles.editor}>
+        <div className={styles.ewrap}>
+          <div className={`bg-transp ${styles.output}`}>
+            <img src={imageURL} />
+          </div>
+          <div className={styles.controls}>
+            <div className={styles.options}>
+              <RangeSlider min={0} max={10} name='contrast' />
+            </div>
+            <div className={styles.tabs}>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
